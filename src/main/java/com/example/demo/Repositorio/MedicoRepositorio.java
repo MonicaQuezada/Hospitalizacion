fc633a7429c3928dd7f1262ada6b4bb43a7dc5bd
Monica Quezada <monica.quezada@ucuenca.edu.ec>
2020-07-12 20:05:47 -0500

    actualizacion

--- src/main/java/com/example/demo/Repositorio/MedicoRepositorio.java
+++ src/main/java/com/example/demo/Repositorio/MedicoRepositorio.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +16,7 @@ public interface MedicoRepositorio extends CrudRepository<Medico,Integer>{
 	public Set<Medico>findAllByCedula(String cedula);
 	public Optional<Medico>findByCedula(String cedula);
 	
-	@Query(value = "SELECT * FROM medico u WHERE u.idEspecialidad = ?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM medico u WHERE u.id_especialidad = ?1", nativeQuery = true)
 	public List<Medico>findByIdEspecialidad(Integer idEspecialidad);
 	
 }
